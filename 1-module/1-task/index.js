782864bd7357c0bbb2df08dd824443c7b8a0e9f1
Anton Alekseev <anton.alekseev@nordigy.ru>
2020-09-08 20:36:44 +0300

    1-module 1-task complete

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -4,5 +4,18 @@
  * @returns {number}
  */
 function factorial(n) {
-  // ваш код...
+
+  if (n === 0) {return 1;}
+
+  let arr = [n];
+
+  for (let i = 1; i < n; i++) {
+    let fac = n - i;
+    arr.push(fac);
+  }
+
+  return arr.reduce(function (a, b) {
+    return a * b;
+  });
+
 }
