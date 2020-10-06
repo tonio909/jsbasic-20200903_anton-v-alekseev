8ae2a3736bde9792b57a77262f88c4afacc79db8
Anton Alekseev <anton.alekseev@nordigy.ru>
2020-10-06 16:07:42 +0300

    5-module 1-task, 2-task, 3-task; 6-module 1-task, 2-task, 3-task complete

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -1,34 +1,41 @@
-/**
- * Компонент, который реализует таблицу
- * с возможностью удаления строк
- *
- * Пример одного элемента, описывающего строку таблицы
- *
- *      {
-     *          name: '',
-     *          age: 25,
-     *          salary: '1000',
-     *          city: 'Petrozavodsk'
-     *   },
- *
- * @constructor
- */
-/**
- * Компонент, который реализует таблицу
- * с возможностью удаления строк
- *
- * Пример одного элемента, описывающего строку таблицы
- *
- *      {
- *          name: 'Ilia',
- *          age: 25,
- *          salary: '1000',
- *          city: 'Petrozavodsk'
- *      },ы
- *
- * @constructor
- */
 export default class UserTable {
   constructor(rows) {
+    this.elem = this.renderTable(rows);
+    this.deleteRow();
+  }
+
+  renderTable(rows) {
+    let table = document.createElement('table');
+    let tableHeader = '<thead><tr>' +
+      '<th>Имя</th>' +
+      '<th>Возраст</th>' +
+      '<th>Зарплата</th>' +
+      '<th>Город</th>' +
+      '<th></th>' +
+      '</tr></thead>';
+    table.insertAdjacentHTML('afterbegin', tableHeader);
+
+    let tableBody = document.createElement('tbody');
+
+    rows.forEach(element => {let row = `<tr>
+        <td>${element.name}</td>
+        <td>${element.age}</td>
+        <td>${element.salary}</td>
+        <td>${element.city}</td>
+        <td><button>X</button></td>
+        </tr>`;
+
+    tableBody.insertAdjacentHTML('beforeend', row);
+
+    });
+
+    table.append(tableBody);
+
+    return table;
+  }
+
+  deleteRow() {
+    let table = this.elem;
+    table.addEventListener('click', (event) => event.target.nodeName === 'BUTTON' && event.target.innerText === 'X' && event.target.closest('tr').remove());
   }
 }
--- 6-module/3-task/index.js
+++ 6-module/3-task/index.js
@@ -1,7 +1,78 @@
-import createElement from '../../assets/lib/create-element.js';
-
 export default class Carousel {
   constructor(slides) {
     this.slides = slides;
+    this.slides = slides;
+    this.elem = this.card(slides);
+    this.event();
+    this.carousel(slides.length);
+  }
+
+  card(slides) {
+    let html = document.createElement('div');
+    html.classList.add('carousel');
+
+    html.insertAdjacentHTML('afterbegin', '<div class="carousel__arrow carousel__arrow_right"><img src="/assets/images/icons/angle-icon.svg" alt="icon"></div><div class="carousel__arrow carousel__arrow_left"><img src="/assets/images/icons/angle-left-icon.svg" alt="icon"></div>');
+
+    let carousel = document.createElement('div');
+    carousel.classList.add('carousel__inner');
+
+    slides.forEach(slide => {
+      let carous = `
+        <div class="carousel__slide" data-id="${slide.id}">
+        <img src="/assets/images/carousel/${slide.image}" class="carousel__img" alt="slide">
+        <div class="carousel__caption">
+          <span class="carousel__price">€${slide.price.toFixed(2)}}</span>
+          <div class="carousel__title">${slide.name}</div>
+          <button type="button" class="carousel__button">
+            <img src="/assets/images/icons/plus-icon.svg" alt="icon">
+          </button>
+        </div>
+      </div>`;
+      carousel.insertAdjacentHTML('beforeend', carous);
+    });
+    html.append(carousel);
+    return html;
+  }
+
+  carousel(nSlidesLength) {
+    let slide = 1;
+    let left = this.elem.querySelector('.carousel__arrow_left');
+    let right = this.elem.querySelector('.carousel__arrow_right');
+    let carousel = this.elem.querySelector('.carousel__inner');
+
+    if (slide === 1) {left.style.display = 'none';} else {left.style.display = '';}
+
+    if (slide === nSlidesLength) {right.style.display = 'none';} else {right.style.display = '';}
+
+    right.addEventListener('click', click);
+    left.addEventListener('click', click);
+
+    function click(event) {
+      let offsetWidth = carousel.offsetWidth;
+      let isLeft = false;
+      if (event.currentTarget === left) {isLeft = true;}
+
+      carousel.style.transform = `translateX(-${(isLeft ? (slide - 2) : slide) * offsetWidth}px)`;
+
+      if (isLeft) {slide--;} else {slide++;}
+
+      if (slide === 1) {left.style.display = 'none';} else {left.style.display = '';}
+      if (slide === nSlidesLength) {right.style.display = 'none';} else {right.style.display = '';}
+    }
+  }
+
+  event() {
+    let element = this.elem;
+    element.addEventListener('click', (event) => {
+      if (event.target.closest('button')) {
+        event.stopPropagation();
+        let id = event.target.closest('.carousel__slide').dataset.id;
+        let productAdd = new CustomEvent('product-add', {
+          detail: id,
+          bubbles: true
+        });
+        element.dispatchEvent(productAdd);
+      }
+    });
   }
 }
